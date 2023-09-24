154b95c3ee21a40a5a1f2cdd1cfb75cbd3d57c78
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-09-24 14:24:42 -0400

    Use different method for void bat logic

--- Common/src/main/java/com/nyfaria/spookybats/entity/VoidBat.java
+++ Common/src/main/java/com/nyfaria/spookybats/entity/VoidBat.java
@@ -1,5 +1,6 @@
 package com.nyfaria.spookybats.entity;
 
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ambient.Bat;
@@ -11,13 +12,12 @@ public class VoidBat extends SpookyBat {
 	}
 
 	@Override
-	public void tick() {
-		super.tick();
-
+	public boolean hurt(DamageSource $$0, float $$1) {
 		LivingEntity attacker = this.getLastHurtByMob();
-		if (attacker != null) {
-			if (attacker.level().dimension() != Level.END)
-				attacker.changeDimension(attacker.getServer().getLevel(Level.END));
-		}
+		boolean canTeleport = attacker != null && attacker.level().dimension() != Level.END;
+		if (canTeleport)
+			attacker.changeDimension(attacker.getServer().getLevel(Level.END));
+
+		return super.hurt($$0, $$1);
 	}
 }
