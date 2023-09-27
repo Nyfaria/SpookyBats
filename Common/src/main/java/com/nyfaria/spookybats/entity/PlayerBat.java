40bcfdf8e30f7dda1c62e65fa50a4247bbf8429e
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-09-27 19:21:28 -0400

    Finally fix player bat AI

--- Common/src/main/java/com/nyfaria/spookybats/entity/PlayerBat.java
+++ Common/src/main/java/com/nyfaria/spookybats/entity/PlayerBat.java
@@ -5,6 +5,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.AvoidEntityGoal;
+import net.minecraft.world.entity.ai.goal.OcelotAttackGoal;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
@@ -18,7 +19,13 @@ public class PlayerBat extends SpookyBat {
 	protected void registerGoals() {
 		super.registerGoals();
 		this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, Monster.class, 7f, 1, 1.4f));
-		this.targetSelector.addGoal(0, new HurtByTargetGoal(this, Monster.class));
+		this.goalSelector.addGoal(0, new OcelotAttackGoal(this));
+		this.targetSelector.addGoal(0, new HurtByTargetGoal(this));
+	}
+
+	@Override
+	public boolean isAggressive() {
+		return this.getLastHurtByMob() != null;
 	}
 
 	public static AttributeSupplier.Builder createPlayerBatAttributes() {
