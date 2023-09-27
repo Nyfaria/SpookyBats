ca17688d610a072928f2e9fc3394832fca41f198
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-09-27 19:06:56 -0400

    Add missing attack damage modifier for player bats

--- Common/src/main/java/com/nyfaria/spookybats/entity/PlayerBat.java
+++ Common/src/main/java/com/nyfaria/spookybats/entity/PlayerBat.java
@@ -2,6 +2,8 @@ package com.nyfaria.spookybats.entity;
 
 import com.nyfaria.spookybats.entity.api.SpookyBat;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.AvoidEntityGoal;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
@@ -17,5 +19,11 @@ public class PlayerBat extends SpookyBat {
 		super.registerGoals();
 		this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, Monster.class, 7f, 1, 1.4f));
 		this.targetSelector.addGoal(0, new HurtByTargetGoal(this, Monster.class));
+	}
+
+	public static AttributeSupplier.Builder createPlayerBatAttributes() {
+		return SpookyBat
+			.createBatAttributes()
+			.add(Attributes.ATTACK_DAMAGE, 1);
 	}
 }
