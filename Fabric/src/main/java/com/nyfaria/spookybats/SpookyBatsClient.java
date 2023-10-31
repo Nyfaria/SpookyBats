38160aaf99bad7b096a291474cd2ff0ff8dac4c8
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-10-31 17:47:37 -0400

    Add display settings to cores

--- Fabric/src/main/java/com/nyfaria/spookybats/SpookyBatsClient.java
+++ Fabric/src/main/java/com/nyfaria/spookybats/SpookyBatsClient.java
@@ -1,17 +1,11 @@
 package com.nyfaria.spookybats;
 
 import com.nyfaria.spookybats.client.CommonClientClass;
-import com.nyfaria.spookybats.client.model.*;
-import com.nyfaria.spookybats.client.renderer.*;
-import com.nyfaria.spookybats.client.renderer.api.SpookyBatRenderer;
 import com.nyfaria.spookybats.client.renderer.layer.BatWingsLayer;
-import com.nyfaria.spookybats.init.EntityInit;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.LivingEntityFeatureRendererRegistrationCallback;
-import net.minecraft.client.model.geom.builders.CubeDeformation;
-import net.minecraft.client.renderer.entity.ThrownItemRenderer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.world.entity.EntityType;
 
