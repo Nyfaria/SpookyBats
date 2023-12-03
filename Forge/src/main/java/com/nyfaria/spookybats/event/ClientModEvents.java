96e09434c3a92ed0f7a4525e60d8a6efd81dad89
Nyfaria DragonMoose <kinaria3324@gmail.com>
2023-12-03 16:44:07 -0600

    fix aws for fabric
    Took 29 minutes

--- Forge/src/main/java/com/nyfaria/spookybats/event/ClientModEvents.java
+++ Forge/src/main/java/com/nyfaria/spookybats/event/ClientModEvents.java
@@ -10,6 +10,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientModEvents {
