eaef7ca445e83342c302dec7b6f7b0103eae4ab0
Nyfaria DragonMoose <kinaria3324@gmail.com>
2023-12-23 11:30:44 -0600

    loot tables and some code cleanup
    Took 20 minutes

--- Common/src/main/java/com/nyfaria/batsgalore/client/renderer/UndeadBatRenderer.java
+++ Common/src/main/java/com/nyfaria/batsgalore/client/renderer/UndeadBatRenderer.java
@@ -7,29 +7,30 @@ import com.nyfaria.batsgalore.client.renderer.api.ModBatRenderer;
 import com.nyfaria.batsgalore.entity.api.ModBat;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 
 public class UndeadBatRenderer extends ModBatRenderer<ModBat> {
-	public static final ResourceLocation FULL_HEALTH_TEXTURE = new ResourceLocation(Constants.MODID, "textures/entity/undead_bat_full.png");
-	public static final ResourceLocation PARTLY_FADED_TEXTURE = new ResourceLocation(Constants.MODID, "textures/entity/undead_bat_part_faded.png");
-	public static final ResourceLocation FULLY_FADED_TEXTURE = new ResourceLocation(Constants.MODID, "textures/entity/undead_bat_faded.png");
+    public static final ResourceLocation FULL_HEALTH_TEXTURE = new ResourceLocation(Constants.MODID, "textures/entity/undead_bat_full.png");
+    public static final ResourceLocation PARTLY_FADED_TEXTURE = new ResourceLocation(Constants.MODID, "textures/entity/undead_bat_part_faded.png");
+    public static final ResourceLocation FULLY_FADED_TEXTURE = new ResourceLocation(Constants.MODID, "textures/entity/undead_bat_faded.png");
 
-	public UndeadBatRenderer(EntityRendererProvider.Context context, ModBatModel<ModBat> model) {
-		super(context, model);
-	}
+    public UndeadBatRenderer(EntityRendererProvider.Context context, ModBatModel<ModBat> model) {
+        super(context, model);
+    }
 
-	@Override
-	protected void scale(ModBat pLivingEntity, PoseStack pMatrixStack, float pPartialTickTime) {
-		pMatrixStack.pushPose();
-		pMatrixStack.scale(1.2f, 1.2f, 1.2f);
-		pMatrixStack.popPose();
-	}
+    @Override
+    protected void scale(ModBat pLivingEntity, PoseStack pMatrixStack, float pPartialTickTime) {
+        pMatrixStack.pushPose();
+        pMatrixStack.scale(1.2f, 1.2f, 1.2f);
+        pMatrixStack.popPose();
+    }
 
-	@Override
-	public ResourceLocation getTextureLocation(ModBat bat) {
-		return switch((int) (bat.getHealth())) {
-			case 6, 5 -> FULL_HEALTH_TEXTURE;
-			case 4, 3, 2 -> PARTLY_FADED_TEXTURE;
-			default -> FULLY_FADED_TEXTURE;
-		};
-	}
+    @Override
+    public ResourceLocation getTextureLocation(ModBat bat) {
+        return switch (Mth.floor((bat.getHealth()))) {
+            case 6, 5 -> FULL_HEALTH_TEXTURE;
+            case 4, 3, 2 -> PARTLY_FADED_TEXTURE;
+            default -> FULLY_FADED_TEXTURE;
+        };
+    }
 }
--- Forge/src/main/java/com/nyfaria/batsgalore/datagen/ModEntityLootTables.java
+++ Forge/src/main/java/com/nyfaria/batsgalore/datagen/ModEntityLootTables.java
@@ -1,8 +1,7 @@
 package com.nyfaria.batsgalore.datagen;
 
-import com.nyfaria.batsgalore.init.entity.ChristmasBatEntityInit;
-import com.nyfaria.batsgalore.init.entity.EntityInit;
 import com.nyfaria.batsgalore.init.ItemInit;
+import com.nyfaria.batsgalore.init.entity.ChristmasBatEntityInit;
 import com.nyfaria.batsgalore.init.entity.SpookyBatEntityInit;
 import net.minecraft.data.loot.EntityLootSubProvider;
 import net.minecraft.world.entity.EntityType;
@@ -13,6 +12,7 @@ import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.entries.LootPoolSingletonContainer;
 import net.minecraft.world.level.storage.loot.functions.FillPlayerHead;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
@@ -64,6 +64,8 @@ public class ModEntityLootTables extends EntityLootSubProvider {
                 new LootEntry(ItemInit.SLIME_CORE.get(), 0.01f, ConstantValue.exactly(1))
         );
         dropSingle(SpookyBatEntityInit.WINGED_TURMOIL.get(), ItemInit.STAFF_OF_TURMOIL.get());
+        dropSetAmountWithChance(ChristmasBatEntityInit.ELF_BAT.get(), ConstantValue.exactly(1f), ItemInit.ELF_HAT.get(), 1, 0.03f);
+        dropSetAmountWithChance(ChristmasBatEntityInit.REINDEER_BAT.get(), ConstantValue.exactly(1f), ItemInit.REINDEER_NOSE.get(), 1, 0.03f);
     }
 
     private void multiDrops(EntityType<?> type, NumberProvider rolls, LootEntry... entries) {
@@ -94,21 +96,32 @@ public class ModEntityLootTables extends EntityLootSubProvider {
     }
 
     private void dropSingle(EntityType<?> entityType, Item item) {
-        TYPES.add(entityType);
-        dropSetAmount(entityType, item, 1);
+        dropSingleWithChance(entityType, item, 0.99f);
+    }
+
+    private void dropSingleWithChance(EntityType<?> entityType, Item item, float chance) {
+        dropSetAmountWithChance(entityType, ConstantValue.exactly(1f), item, 1, chance);
     }
 
     private void dropSetAmount(EntityType<?> entityType, Item item, float amount) {
+        dropSetAmountWithChance(entityType, ConstantValue.exactly(1f), item, amount, 0.99f);
+    }
+
+    private void dropSetAmountWithChance(EntityType<?> entityType, NumberProvider rolls, Item item, float amount, float chance) {
+        TYPES.add(entityType);
         LootTable.Builder builder = LootTable.lootTable();
-        builder.withPool(LootPool.lootPool()
-                .setRolls(ConstantValue.exactly(1))
-                .add(LootItem.lootTableItem(item)
-                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(amount))))
-                .add(LootItem.lootTableItem(ItemInit.BAT_WINGS.get())
-                        .when(LootItemRandomChanceCondition.randomChance(0.01f))
-                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))
-                )
-        );
+        LootPool.Builder pool = LootPool.lootPool()
+                .setRolls(rolls);
+        LootPoolSingletonContainer.Builder<?> lootItem = LootItem.lootTableItem(item);
+        if (chance != 0.99f) {
+            lootItem = lootItem.when(LootItemRandomChanceCondition.randomChance(chance));
+        }
+        pool.add(lootItem
+                .apply(SetItemCountFunction.setCount(ConstantValue.exactly(amount))));
+        pool.add(LootItem.lootTableItem(ItemInit.BAT_WINGS.get())
+                .when(LootItemRandomChanceCondition.randomChance(0.01f))
+                .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1))));
+        builder.withPool(pool);
         add(entityType, builder);
     }
 
